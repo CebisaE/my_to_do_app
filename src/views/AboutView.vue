92dab29994e65225e0040914439642f32da8dff8
emihle <mihlekcebisa@gmail.com>
2022-07-18 22:33:02 +0200

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-9">
+    <h1>About To-Do-App</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+      Repellendus earum molestias delectus laboriosam qui nam debitis ullam tempora eaque nesciunt, 
+      voluptatibus quia et vero aspernatur eligendi dignissimos provident optio maxime.
+      </p>
+    <p>Lorem ipsum dolor sit amet consectetur,
+       adipisicing elit. Voluptate non, 
+       nisi officia deserunt facilis, 
+      ratione illum consectetur inventore doloremque expedita,
+       fuga numquam rem eos! Dicta dolorem ad repellendus facere culpa.
+    </p>
   </div>
+
 </template>
---
+++ src/views/ToDo.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="home">
+    <v-list
+    
+      two-line
+      flat
+    >
+        <v-list-item>
+          <template v-slot:default="{ active, }">
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="active"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>Notifications</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+    </v-list>
+</div>
+</template>
+
+<script>
+
+
+  export default {
+    name: 'Home',
+
+   
+  }
+</script>
