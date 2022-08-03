c5817868b093f2cb6e6113065846f878498cd548
emihle <mihlekcebisa@gmail.com>
2022-08-03 16:21:43 +0200

    changes

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -1,24 +1,55 @@
 <template>
 <div class="home">
+        <v-text-field
+            v-model="newTaskTitle"
+            @click:append="addTask"
+            @keyup.enter="addTask"
+            class="pa-3"
+            outlined
+            label="Add A Task"
+            append-icon="mdi-plus"
+            hide-details
+            clearable
+          ></v-text-field>
+
     <v-list
-    
-      two-line
-      flat
+    class="pt-0"
+     flat
     >
-        <v-list-item>
-          <template v-slot:default="{ active, }">
+    <div
+    v-for="task in tasks"
+          :key="task.id">
+        <v-list-item
+        @click="doneTask(task.id)"
+        :class="{'blue  lighten-5' : task.done}"
+        >
+          <template v-slot:default>
             <v-list-item-action>
               <v-checkbox
-                :input-value="active"
+                :input-value="task.done"
                 color="primary"
               ></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>Notifications</v-list-item-title>
+              <v-list-item-title
+              :class="{'text-decoration-line-through' : task.done }"
+              >
+                {{task.title}}
+                </v-list-item-title>
             </v-list-item-content>
+            <v-btn 
+            @click.stop="deleteTask(task.id)"
+            icon
+            >
+             <v-icon color="primary lighten-1">
+                  mdi-delete
+                </v-icon>
+                </v-btn >
           </template>
         </v-list-item>
+        <v-divider></v-divider>
+        </div>
     </v-list>
 </div>
 </template>
@@ -28,7 +59,46 @@
 
   export default {
     name: 'Home',
+    data(){
+      return{
+        newTaskTitle:'',
+        tasks:[
+          // {
+          //   id: 1,
+          //   title: 'Wake Up',
+          //   done:false
+          // },
+          // {
+          //   id: 2,
+          //   title: 'Make The Bed',
+          //   done:false
+          // },
+          // {
+          //   id: 3,
+          //   title: 'Wash',
+          //   done:false
+          // }
+        ]
+      }
 
-   
+    },
+  methods: {
+    addTask(){
+    let newTask = {
+      id:Date.now(),
+      title:this.newTaskTitle,
+      done:false
+    }
+    this.tasks.push(newTask)
+    this.newTaskTitle = ''
+    },
+   doneTask(id){
+     let task = this.tasks.filter(task => task.id === id)[0]
+     task.done = !task.done     
+   },
+   deleteTask(id){
+     this.tasks = this.tasks.filter(task => task.id !== id)
+   }
+  }
   }
 </script>
